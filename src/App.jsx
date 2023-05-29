a555f86441116f9bcc0f115d28eff7dddefe2e6d
rafeuddaraj <rafeuddaraj2@gmail.com>
2023-05-30 01:58:36 +0600

    update some fetured, version 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Countdown from './coming';
 
 const App = () => {
-  const targetDate = new Date('2023-07-01T00:00:00').getTime();
+  const targetDate = new Date('2023-08-18T00:00:00').getTime();
 
   return (
     <div>
